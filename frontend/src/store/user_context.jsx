cd6b32f0618cf8753c7616e4eea1a67d3dbf1e03
gillesretiere <gilles.retiere@gmail.com>
2025-12-03 14:58:18 +0000

    Intégration new JSON files (PT+ES+AR)

--- frontend/src/store/user_context.jsx
+++ frontend/src/store/user_context.jsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 export const UserContext = createContext({
     decks: [],
-    matrix: ['rec7lQw6DtIJP45dn-fre', 'rec7lQw6DtIJP45dn-rom', 'rec7lQw6DtIJP45dn-rus', 'rec7lQw6DtIJP45dn-ams',
+    matrix: ['rec7lQw6DtIJP45dn-fre', 'rec7lQw6DtIJP45dn-rom', 'rec7lQw6DtIJP45dn-rus', 'rec7lQw6DtIJP45dn-ams', 'rec7lQw6DtIJP45dn-spa', 'rec7lQw6DtIJP45dn-por',
         'rec4nBx7PmDA3Y7Ji-fre', 'rec4nBx7PmDA3Y7Ji-ams',
         'reclNHMgbbYroSaNq-fre', 'reclNHMgbbYroSaNq-ams',
         'rec6ogwslaOOKRmRI',
