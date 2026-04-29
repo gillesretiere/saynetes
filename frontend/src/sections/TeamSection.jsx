8cf5adea248a5d147de1d75373a0c683f5ac83fa
gillesretiere <gilles.retiere@gmail.com>
2026-04-29 13:47:43 +0000

    Pré-release 20260430

--- frontend/src/sections/TeamSection.jsx
+++ frontend/src/sections/TeamSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { interpretes, mariama, mohamed, nune, paula, sonia, alysone, } from "../assets/img/index.js";
+import { interpretes, mariama, mohamed, nune, paula, sonia, alysone, gulseren, } from "../assets/img/index.js";
 
 const TeamSection = () => {
   // Simulation d'une liste d'interprètes (à remplir avec vos photos réelles)
@@ -10,6 +10,7 @@ const TeamSection = () => {
     { name: "Sonia", lang: "Espagnol", img: sonia },
     { name: "Alysone", lang: "Anglais", img: alysone },
     { name: "Mohamed", lang: "Arabe", img: mohamed },
+    { name: "Gulseren", lang: "Turque", img: gulseren }
     // Ajoutez autant d'entrées que nécessaire...
   ];
 
--- frontend/src/store/user_context.jsx
+++ frontend/src/store/user_context.jsx
@@ -4,8 +4,8 @@ export const UserContext = createContext({
     decks: [],
     matrix: [
         'rec7lQw6DtIJP45dn-fre', 'rec7lQw6DtIJP45dn-rom', 'rec7lQw6DtIJP45dn-rus', 'rec7lQw6DtIJP45dn-ams', 'rec7lQw6DtIJP45dn-spa','rec7lQw6DtIJP45dn-por','rec7lQw6DtIJP45dn-eng', 'rec7lQw6DtIJP45dn-tur',
-        'rec4nBx7PmDA3Y7Ji-fre', 'rec4nBx7PmDA3Y7Ji-ams', 'rec4nBx7PmDA3Y7Ji-por', 'rec4nBx7PmDA3Y7Ji-eng', 'rec4nBx7PmDA3Y7Ji-rus', 'rec4nBx7PmDA3Y7Ji-rom',
-        'reclNHMgbbYroSaNq-fre', 'reclNHMgbbYroSaNq-ams', 'reclNHMgbbYroSaNq-rus', 'reclNHMgbbYroSaNq-rom',
+        'rec4nBx7PmDA3Y7Ji-fre', 'rec4nBx7PmDA3Y7Ji-ams', 'rec4nBx7PmDA3Y7Ji-por', 'rec4nBx7PmDA3Y7Ji-eng', 'rec4nBx7PmDA3Y7Ji-rus', 'rec4nBx7PmDA3Y7Ji-rom', 'rec4nBx7PmDA3Y7Ji-tur',
+        'reclNHMgbbYroSaNq-fre', 'reclNHMgbbYroSaNq-ams', 'reclNHMgbbYroSaNq-rus', 'reclNHMgbbYroSaNq-rom', 
         'rec6ogwslaOOKRmRI',
         'recv3YSPQJmjKZEBy',]
 });
